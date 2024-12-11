42c2f101e3e1aef910fadc04dd8df2b890f17b23
TRhodesDEV <travis.rhodes9@gmail.com>
2024-12-10 22:28:35 -0500

    Remove disliked movie array from user class

--- src/User.java
+++ src/User.java
@@ -5,15 +5,13 @@ public class User {
 
         public String username;
         public List<Movie> likedMovies;
-        public List<Movie> dislikedMovies;
 
-        public User(String username){
+        public User(String username) {
                 this.username = username;
                 this.likedMovies = new ArrayList<>();
-                this.dislikedMovies = new ArrayList<>();
         }
 
-        public String getUsername(){
+        public String getUsername() {
                 return username;
         }
 
@@ -21,15 +19,9 @@ public class User {
                 return likedMovies;
         }
 
-        public List<Movie> getDislikedMovies() {
-                return dislikedMovies;
-        }
-
-        public void addLikedMovie(Movie movie){
+        public void addLikedMovie(Movie movie) {
                 likedMovies.add(movie);
         }
 
-        public void addDislikedMovie(Movie movie){
-                dislikedMovies.add(movie);
-        }
+
 }
